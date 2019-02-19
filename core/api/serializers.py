1e63ad411f027b5facb51f16428221be165ab430
Timponi <gabrieltimponi@gmail.com>
2019-02-19 12:05:31 -0300

    Create atracoes

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.fields import SerializerMethodField
-from core.models import PontoTuristico
+from core.models import PontoTuristico, Atracao
 from atracoes.api.serializers import AtracaoSerializer
 from endereco.api.serializers import EnderecosSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
-    atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecosSerializer()
+    atracoes = AtracaoSerializer(many=True, read_only=True)
+    endereco = EnderecosSerializer(read_only=True)
     descricao_completa = SerializerMethodField()
 
     class Meta:
@@ -17,6 +17,9 @@ class PontoTuristicoSerializer(ModelSerializer):
             'atracoes', 'comentarios', 'avaliacao', 'endereco',
             'descricao_completa', 'descricao_completa2'
             )
+        read_only_fields = ('comentarios', 'avaliacao')
+
+   
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
